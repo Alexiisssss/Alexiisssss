33f976cb2d437a21895cde98330f710564e375a9
Alexiisssss <q09w12@mail.ru>
2024-03-19 22:18:44 +0500

    My twenty task learning in Python

---
+++ main20.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+
+for page_num in range(1, 16):
+    url = f"https://2051.vision/category/ii/page/{page_num}"
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'html5lib')
+
+    print(f"Страница {page_num}:")
+    for h3 in soup.find_all('h3'):
+        print(h3.text.strip())
+    print("\n")
